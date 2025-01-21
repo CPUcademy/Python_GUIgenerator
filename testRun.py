f363098a217c0161e421b64998110d0b3849fa38
Tymoteusz Kołodziejczyk <cpucademy@gmail.com>
2025-01-21 13:05:50 +0100

    Update testRun.py

--- testRun.py
+++ testRun.py
@@ -1,10 +1,10 @@
-from GUIgenerator import *
+from gui_generator import *
 
 def Average(n):
     s = 0
     for x in range(n):
-        s += int(g.addInput())
+        s += int(g.addInput("Enter a value:"))
     return s / n
 
 g = GUIgenerator()
-g.create(Average, args=["How many numbers:"])
+g.create(Average, args=["How many numbers:"], desc="Calculate the average value.")
